1bf812e940142d0df6af16b0adb9b9eb10ad92eb
TellesWar <53920499+telleswar@users.noreply.github.com>
2024-12-06 03:42:10 +0000

    sidebar

--- templates/static/general/js/base.js
+++ templates/static/general/js/base.js
@@ -1,5 +1,11 @@
 function toggleSidebar() {
   const sidebar = document.getElementById("sidebar");
-  sidebar.classList.toggle("hidden");
-  sidebar.classList.toggle("-translate-x-full");
+  const toggle_x = document.getElementById("toggle-x");
+  const toggle_hambuguer = document.getElementById("toggle-hambuguer");
+
+  sidebar.classList.toggle("w-64");
+
+  toggle_x.classList.toggle("hidden");
+  toggle_hambuguer.classList.toggle("hidden");
+
 }
