14f3e4ac0bd018df2fccff698fea6db1352d0172
TellesWar <53920499+telleswar@users.noreply.github.com>
2024-12-06 12:05:10 +0000

    feat: Opções de navegação principais adicionadas na sidebar

--- setup/urls.py
+++ setup/urls.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     # Login e Logout
+    path(
+        "",
+        RedirectView.as_view(url="/login/", permanent=False),
+        name="redirect_view",
+    ),
     path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),
     path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
     path("admin/", admin.site.urls),
--- templates/static/general/js/base.js
+++ templates/static/general/js/base.js
@@ -2,10 +2,17 @@ function toggleSidebar() {
   const sidebar = document.getElementById("sidebar");
   const toggle_x = document.getElementById("toggle-x");
   const toggle_hambuguer = document.getElementById("toggle-hambuguer");
+  const nav_item_name_list = document.querySelectorAll("span.nav-item-name");
 
   sidebar.classList.toggle("w-64");
+  sidebar.classList.toggle("md:hover:w-64");
+  sidebar.classList.toggle("w-20");
 
   toggle_x.classList.toggle("hidden");
   toggle_hambuguer.classList.toggle("hidden");
+
+  nav_item_name_list.forEach(nav_item_name => {
+    nav_item_name.classList.toggle("hidden");
+  });
 
 }
